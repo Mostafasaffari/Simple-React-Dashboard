bd358dd6a645ae0ed59a5b39950b29c18dcffce4
Mostafa Saffari <mostafasaffari@gmail.com>
2019-10-08 08:26:03 +0330

    add app component

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import App from "./pages/app";
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={() => <h1>home</h1>} />
+        <Route exact path="/" component={App} />
         <Route component={() => <h1>404</h1>} />
       </Switch>
     </BrowserRouter>
