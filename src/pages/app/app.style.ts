e7561aae77efb71d393d0e082d4bf848273e4b88
Mostafa Saffari <mostafasaffari@gmail.com>
2019-10-09 18:02:43 +0330

    remove ts ignore

--- src/pages/app/app.style.ts
+++ src/pages/app/app.style.ts
@@ -1,7 +1,6 @@
 import { makeStyles } from "@material-ui/styles";
 import { Theme } from "@material-ui/core";
 
-//@ts-ignore
 const style = makeStyles((theme: Theme) => {
   return {
     wrapper: {
