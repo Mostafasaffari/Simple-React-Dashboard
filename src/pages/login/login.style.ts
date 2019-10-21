fe74966c35f1cf849cccf1827ad8c569d4055744
Mostafa Saffari <mostafasaffari@gmail.com>
2019-10-21 16:18:25 +0330

    fix: bug fix in useStyle in FC

--- src/pages/login/login.style.ts
+++ src/pages/login/login.style.ts
@@ -4,6 +4,8 @@ const style = makeStyles((theme: Theme) =>
   createStyles({
     wrapper: {
       display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
       width: "100%",
       height: "100vh"
     }
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 import useStyle from "./login.style";
-const classes = useStyle();
 const Login = () => {
+  const classes = useStyle();
   return <div className={classes.wrapper}>hi</div>;
 };
 
